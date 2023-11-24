11d745ec77bca5800e03877dcc4418bebe306dfa
Jeshua Hinostroza <hinostrozajeshua@gmail.com>
2023-11-23 20:00:56 -0500

    feat: create main layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { MainLayoutComponent } from './layout/main-layout/main-layout.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, MainLayoutComponent],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'sales-dashboard';
 }
---
+++ src/app/layout/main-layout/main-layout.component.ts
@@ -0,0 +1,32 @@
+import { Component, ChangeDetectionStrategy, inject, computed } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatDrawerMode, MatSidenavModule } from '@angular/material/sidenav';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { map } from 'rxjs';
+import { toSignal } from '@angular/core/rxjs-interop';
+
+@Component({
+  selector: 'app-main-layout',
+  standalone: true,
+  imports: [CommonModule, MatSidenavModule, MatButtonModule, MatIconModule],
+  templateUrl: './main-layout.component.html',
+  styleUrls: ['./main-layout.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  host: {
+    class: 'block relative',
+  },
+})
+export class MainLayoutComponent {
+  protected small = toSignal(
+    inject(BreakpointObserver)
+     .observe([Breakpoints.Small, Breakpoints.XSmall])
+      .pipe(map((x) => x.matches)),
+    {
+      requireSync: true,
+    }
+  );
+
+  protected drawerMode = computed((): MatDrawerMode => this.small() ? 'over' : 'side');
+}
