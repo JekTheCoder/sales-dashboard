53301fe12a2ddc56a7a1c992baf63e61e02be913
Jeshua Hinostroza <hinostrozajeshua@gmail.com>
2023-11-25 18:09:19 -0500

    feat(meeting): view more buttons for client info & annotations

--- src/app/pages/meeting/meeting.component.ts
+++ src/app/pages/meeting/meeting.component.ts
@@ -2,11 +2,14 @@ import { Component, ChangeDetectionStrategy } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { meeting } from './meeting.data';
 import { ChatComponent } from './chat/chat.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @Component({
   selector: 'app-meeting',
   standalone: true,
-  imports: [CommonModule, ChatComponent],
+  imports: [CommonModule, ChatComponent, MatButtonModule, MatIconModule, MatTooltipModule],
   templateUrl: './meeting.component.html',
   styleUrls: ['./meeting.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
